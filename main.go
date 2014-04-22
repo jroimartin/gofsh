2632458b395ade21ae8622f843cbe7ea2454258d
Roi Martin (@nibble_ds) <jroi.martin@gmail.com>
2014-04-22 23:54:41 +0200

    Add logging capabilities

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-// Copyright 2014 The gofsh Authors.  All rights reserved.
+// Copyright 2014 The gofsh Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
@@ -18,7 +18,13 @@ var (
 func main() {
 	flag.Parse()
 	log.Printf("Listening on %s (root=%s)\n", *addr, *root)
-	if err := http.ListenAndServe(*addr, http.FileServer(http.Dir(*root))); err != nil {
+	if err := http.ListenAndServe(*addr, http.HandlerFunc(logHandler)); err != nil {
 		log.Fatalln("Error:", err)
 	}
+}
+
+func logHandler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("%s - %s\n", r.RemoteAddr, r.RequestURI)
+	fileHandler := http.FileServer(http.Dir(*root))
+	fileHandler.ServeHTTP(w, r)
 }
